fab89cceafbdde293161f01dfc91bda209953c76
vagrant <vagrant@localhost.localdomain>
2019-09-23 09:41:10 +0900

    connect DB

--- app.rb
+++ app.rb
@@ -1,33 +1,18 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+	adapter: 'sqlite3',
+	database: './bbs.db'
+)
+
+class Comment < ActiveRecord::Base
+end
 
 ## htmlを表示
 get '/' do
-    @title = "My Site"
-    erb :index
+	@title = "My BBS"
+	@comments = Comment.all
+	erb :index
 end
-
-get '/about' do
-    @title = "About This Site"
-    erb :about
-end
-
-# get '/hello/:name' do
-#     "hello #{params[:name]}"
-# end
-
-# get '/hello/:name' do |name|
-#     "hello #{name}"
-# end
-
-# get '/hello/:fname/?:lname?' do |f,l|
-#     "hello #{f} #{l}"
-# end
-
-# get '/hello/*/*' do
-#     "hello #{params[:splat][0]} #{params[:splat][1]}"
-# end
-
-# get %r{/users/([0-9]*)} do
-#     "user id = #{params[:captures][0]}"
-# end
