0abfdc1c5791f1616c14b3661df9d69a1a5023fb
vagrant <vagrant@localhost.localdomain>
2019-09-23 11:21:56 +0900

    refactor:use helper

--- app.rb
+++ app.rb
@@ -6,6 +6,18 @@ require 'rack/csrf'
 use Rack::Session::Cookie, secret:"Thisissomthingsecret"
 use Rack::Csrf, raise:true
 
+helpers do
+	def csrf_tag
+		Rack::Csrf.csrf_tag(env)
+	end
+	def csrf_token
+		Rack::Csrf.csrf_token(env)
+	end
+	def h(str)
+		Rack::Utils.escape_html(str)
+	end
+end
+
 ActiveRecord::Base.establish_connection(
 	adapter: 'sqlite3',
 	database: './bbs.db'
